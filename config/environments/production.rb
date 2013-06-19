68671811c0d49c49052d7eafbfaf5e3c1afb807d
Mario A Chavez <mario.chavez@gmail.com>
2013-06-18 23:14:41 -0500

    Change delivery method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,9 +43,6 @@ Discourse::Application.configure do
       :authentication       => 'plain',
       :enable_starttls_auto => true  }
 
-    config.action_mailer.delivery_method = :sendmail
-    config.action_mailer.sendmail_settings = {arguments: '-i'}
-
     # Send deprecation notices to registered listeners
     config.active_support.deprecation = :notify
 
